6d211b92dfa032fecd3c5c73a6c3265ee19c1d33
GX789123 <63955747+GX789123@users.noreply.github.com>
2021-01-04 07:45:09 +0300

    Update hamis.py

--- hamis.py
+++ hamis.py
@@ -10,7 +10,11 @@ print(cars[1])
 "
 cars = ['bmw' ,'merceds','alfaromeo']
 print(cars[-1])
-
+"
 
 print(cars[2:])
 cars = ['bmw' ,'merceds','alfaromeo','feriri','honda']
+"
+
+cars = ['bmw' ,'merceds','alfaromeo','feriri','honda']
+print(cars[:2])
