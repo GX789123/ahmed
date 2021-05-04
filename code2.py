3ae19c50b86207ffaf4e0f1efa9c6aa9513326c1
GX789123 <63955747+GX789123@users.noreply.github.com>
2021-05-04 10:16:25 +0300

    Delete code2.py

--- code2.py
+++
@@ -1,8 +0,0 @@
-character_name = "mourad"
-age = "50"
-print("there was a boy called " +character_name+  " ahmed")
-print(" he was "+age+" years old ")
-character_name = "ahmed"
-print("he liked his name " +character_name)
-print("but wasn't happy about being "+age)
-print(character_name+ " has one brother")
